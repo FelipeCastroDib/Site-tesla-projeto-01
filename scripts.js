97ad3ac4567860586919c09e5dcbe1b089601c58
Felipe Dib <felipedibcastro@gmail.com>
2024-07-27 14:50:10 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+//document = ir no HTML
+//queryselector = busca o que eu precisar
+let formulario = document.querySelector(".formulario")
+
+function openForm(){
+
+    formulario.style.left = "50%"
+
+}
+
+
+function closeForm(){
+    
+    formulario.style.left = "-50%"
+
+}
